b6b54dd34d8a7845d09f552eda6d837e3dbcbc00
Rene <flughoernchen1415@gmail.com>
2018-06-22 14:50:49 +0200

    Testing the RestTemplate by using an example api

--- src/main/java/reichhorn/spring/restclient/service/ApiServiceImpl.java
+++ src/main/java/reichhorn/spring/restclient/service/ApiServiceImpl.java
@@ -1,7 +1,9 @@
 package reichhorn.spring.restclient.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 import reichhorn.spring.api.model.User;
 import reichhorn.spring.api.model.UserData;
 
@@ -12,14 +14,22 @@ public class ApiServiceImpl implements ApiService {
 
     private RestTemplate restTemplate;
 
-    public ApiServiceImpl(RestTemplate restTemplate) {
+    // api url is externalised in the application.properties file
+    private final String API_URL;
+
+    public ApiServiceImpl(RestTemplate restTemplate, @Value("${api.url}") String API_URL) {
         this.restTemplate = restTemplate;
+        this.API_URL = API_URL;
     }
 
     @Override
     public List<User> getUsers(Integer limit) {
 
-        UserData userData = restTemplate.getForObject("http://apifaketory.com/api/user?limit=" + limit, UserData.class);
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder
+                .fromUriString(API_URL)
+                .queryParam("limit", limit);
+
+        UserData userData = restTemplate.getForObject(uriBuilder.toUriString(), UserData.class);
 
         return userData.getData();
     }
